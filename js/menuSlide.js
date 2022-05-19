322b6805c51c6421bc7282b4f8ec0b70bdc5a339
Soy_NooV <91479490+soynoov@users.noreply.github.com>
2022-05-19 15:50:43 +0200

    Add files via upload

--- js/menuSlide.js
+++ js/menuSlide.js
@@ -1,10 +1,15 @@
 function menuSlide() {
     document.querySelector(".nav_footer").style.transition = "margin 2s ease";
-    document.querySelector(".nav_footer").style.marginTop = 80 + "vh";
+    document.querySelector(".nav_footer").style.marginTop = 9 + "vh";
     document.querySelector(".nav_footer").style.cursor = "pointer";
+    // Añadir animación de vuelta
+    document.querySelector(".div_main").style.animation = "menuCome 2s ease"; 
 }
 
 function menuSlideReverse() {
-    document.querySelector(".nav_footer").style.marginTop = 3 + "vh";
+    document.querySelector(".nav_footer").style.transition = "margin 2s ease";
+    document.querySelector(".nav_footer").style.marginTop = -71 + "vh";
     document.querySelector(".nav_footer").style.cursor = "default";
+    // Añadir animación de ida
+    document.querySelector(".div_main").style.animation = "menuGone 2s ease"; 
 }
